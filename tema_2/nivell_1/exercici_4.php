2006f3126300cf3c4c51126a51ade2698c2237fd
Oscar <oscanguera@gmail.com>
2024-05-07 13:58:26 +0200

    Corregido otra vezgit add .  exercici 4 del tema 2 nivell 1

--- tema_2/nivell_1/exercici_4.php
+++ tema_2/nivell_1/exercici_4.php
@@ -4,42 +4,18 @@
         $num = readline("Introdueix el número final: \n");
         $comptar = readline("De cuant en cuant vols comptar: \n");
        
-        function calcula2(){
-            $i = 1;
-            while ($i <= 10){
-                echo $i++ . "\n";
-            }
-        }
-        function calcula($comptar, $num){
+      
+        function calcula($comptar = 1, $num =10){
             for($i = 0; $i <= $num; $i += $comptar)
             { 
                 
                 echo $i ." => ". $comptar. "\n";  
             }
         }
-        
-        function calcula3($comptar) {
-            for ($i = 0; $i <= 10; $i += $comptar) {
-                echo $i . "\n";  
-            }
-        }
+        calcula($comptar, $num);
+        calcula();
         
         
-        function imprimir($comptar, $num){
-            if($num == null || $comptar == null)
-            {
-                calcula2();
-            }
-            elseif($num == null && $comptar != null)
-            {
-                calcula3($comptar);
-            }
-            else
-            {
-                calcula($comptar, $num);
-            }
-        }
-        imprimir($comptar, $num);
     
 ?>
 
