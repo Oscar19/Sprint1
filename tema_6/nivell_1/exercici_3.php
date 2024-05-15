8b6ae756263f40aaf87b0b7dc61b3c531d426b67
Oscar <oscanguera@gmail.com>
2024-05-15 11:17:58 +0200

    Corregir ejercicio 3 tema 6 nivel 1

--- tema_6/nivell_1/exercici_3.php
+++ tema_6/nivell_1/exercici_3.php
@@ -1,46 +1,17 @@
 <?php 
-    //Sobreescribir métodos
-    // He partido de el ejercicio 2 del tema 4 de calcular areas
-    class Shape{
-        public $ample, $alçada, $area;
-       
-        public function altura($alçada){
-            $this->alçada = $alçada;
+    class Players{
+        private $name;
+
+        public function __construct($name){
+            $this->name = $name;
         }
-        public function base($ample){
-            $this->ample = $ample;
-        }
-        public function resultat(){
-            echo $this->area;
+
+        public function __toString(){
+            return "Nombre: ". $this->name;
         }
         
     }
+    $player1 = new players('Federico');
 
-    class triangulo extends Shape{
-        public function calcularArea(){
-            $this->area = ($this->alçada * $this->ample)/2;
-        }
-    }
-    class rectangulo extends Shape{ 
-        public function calcularArea(){
-            $this->area = ($this->alçada * $this->ample);
-        }
-        public function resultat(){
-            echo "El area del rectangle que te ".$this->alçada." de alçada i ".$this->ample." de amplada es de: ";
-            parent::resultat();
-        }
-    }
-    $triangulo = new triangulo();
-    $triangulo->altura(10);
-    $triangulo->base(10);
-    $triangulo->calcularArea();
-    echo "El area del triangulo es: ";
-    $triangulo->resultat();
-    echo "<br>";
-
-    $rectangulo = new rectangulo();
-    $rectangulo->altura(15);
-    $rectangulo->base(5);
-    $rectangulo->calcularArea();
-    $rectangulo->resultat();
+    echo $player1;
 ?>
