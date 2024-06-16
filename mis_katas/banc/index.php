5640143ea1e914bf82bb06d0ba4c964fd867ad4a
oscar <oscanguera@gmail.com>
2024-06-16 20:53:37 +0200

    subir otro ejercicio

--- mis_katas/banc/index.php
+++ mis_katas/banc/index.php
@@ -27,48 +27,38 @@ do {
     echo "3.- Crear compte d'un client/a.\n";
     echo "4.- Ingressar euros en un compte d'un client/a.\n";
     echo "5.- Retirar euros en un compte d'un client/a.\n";
-    echo "Trieu una opció: ";
-    $opcio = trim(fgets(STDIN));
+  
+    $opcio = readline("Trieu una opció: ");
 
     switch ($opcio) {
         case 0:
             echo "Sortint de l'aplicació...\n";
             break;
         case 1:
-            echo "Introduïu el DNI del client: ";
-            $dni = trim(fgets(STDIN));
-            echo "Introduïu el nom del client: ";
-            $nom = trim(fgets(STDIN));
+            $dni = readline("Introduïu el DNI del client: ");
+            $dni = readline("Introduïu el nom del client: ");
             $banc->crearClient($dni, $nom);
             break;
         case 2:
-            echo "Introduïu el DNI del client a eliminar: ";
-            $dni = trim(fgets(STDIN));
+            $dni = readline("Introduïu el DNI del client a eliminar: ");
             $banc->eliminarClient($dni);
             break;
         case 3:
-            echo "Introduïu el DNI del client: ";
-            $dni = trim(fgets(STDIN));
-            echo "Introduïu el número de compte: ";
-            $numeroCompte = trim(fgets(STDIN));
+            $dni = readline("Introduïu el DNI del client: ");
+            $numeroCompte = readline("Introduïu el número de compte: ");
             $banc->crearCompte($dni, $numeroCompte);
             break;
         case 4:
-            echo "Introduïu el DNI del client: ";
-            $dni = trim(fgets(STDIN));
-            echo "Introduïu el número de compte: ";
-            $numeroCompte = trim(fgets(STDIN));
-            echo "Introduïu la quantitat a ingressar: ";
-            $quantitat = floatval(trim(fgets(STDIN)));
+            
+            $dni = readline("Introduïu el DNI del client: ");
+            $numeroCompte = readline("Introduïu el número de compte: ");
+            $quantitat = readline("Introduïu la quantitat a ingressar:  ");
             $banc->ingressar($dni, $numeroCompte, $quantitat);
             break;
         case 5:
-            echo "Introduïu el DNI del client: ";
-            $dni = trim(fgets(STDIN));
-            echo "Introduïu el número de compte: ";
-            $numeroCompte = trim(fgets(STDIN));
-            echo "Introduïu la quantitat a retirar: ";
-            $quantitat = floatval(trim(fgets(STDIN)));
+            $dni = readline("Introduïu el DNI del client: ");
+            $numeroCompte = readline("Introduïu el número de compte: ");
+            $quantitat = readline("Introduïu la quantitat a retirar:");
             $banc->retirar($dni, $numeroCompte, $quantitat);
             break;
         default:
